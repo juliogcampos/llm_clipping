949abbb72dcb0b503b63bc9d205c44b49e38cab8
Júlio Gonçalves Campos <juliogcampos@tecgraf.puc-rio.br>
2025-10-02 17:02:45 -0300

    filtering news by source

--- src/js/vue.js
+++ src/js/vue.js
@@ -4,21 +4,50 @@ createApp({
     data() {
         return {
             news: [],
-            searchTerm: ''
+            searchTerm: '',
+            selectedSource: ''
         };
     },
 
     computed: {
+
+        filteredSources() {
+            let sources = new Set();
+            this.news.forEach(item => {
+                if (item.source && item.source.trim() !== '') {
+                    sources.add(item.source);
+                }
+            });
+
+            sources = [...sources]
+            return sources.sort();
+        },
+
         filteredPosts() {
             const term = this.searchTerm.toLowerCase();
-            if (!term) return this.news;
 
-            return this.news.filter(article => {
-                return (
-                    article.title.toLowerCase().includes(term) ||
-                    article.source.toLowerCase().includes(term) ||
-                    article.summary.toLowerCase().includes(term)
-                );
+            return this.news.filter(item => {
+
+                // is valid item
+                if (!item.title || !item.source || !item.date || !item.summary || !item.url) {
+                    return false;
+                }
+
+                // filter by source
+                if (this.selectedSource && item.source !== this.selectedSource) {
+                    return false;
+                }
+
+                // filter by term
+                if (term) {
+                    return (
+                        item.title.toLowerCase().includes(term) ||
+                        item.source.toLowerCase().includes(term) ||
+                        item.summary.toLowerCase().includes(term)
+                    );
+                }
+
+                return true;
             });
         }
     },
@@ -37,7 +66,16 @@ createApp({
             } catch (err) {
                 console.error("Error to load JSON:", err);
             }
+        },
+
+        selectSource(source) {
+            this.selectedSource = source;
+        },
+
+        clearSource() {
+            this.selectedSource = '';
         }
+
     },
 
     mounted() {
